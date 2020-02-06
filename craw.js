fb851e3c1c4eb3c9c47a75d0ef48f55a64f02bb6
haubar <haubarclack@gmail.com>
2020-02-06 18:12:28 +0800

    加入執行變數判斷

--- craw.js
+++ craw.js
@@ -31,71 +31,6 @@ async function writeDB (title, url, tablename) {
     })
 }
 
-/*
-async function getPic() {
-  //啟動一個puppeteer環境
-  const browser = await puppeteer.launch();
-  //開啟新瀏覽頁面
-  const page = await browser.newPage();
-  //指定頁面網址
-  await page.goto(process.env.test_url);
-  //截圖
-  await page.screenshot({path: 'google.png'});
-  //關閉環境
-  await browser.close();
-}
-
-getPic();
-*/
-
-// --------------------------------------------------------------------------
-
-/*
-puppeteer sample.....
-
-let scrape = async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-
-    await page.goto(process.env.demo_url);
-    // await page.click('#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a > img');
-    // await page.waitFor(1000);
-
-    // const result = await page.evaluate(() => {
-    //     let title = document.querySelector('h1').innerText;
-    //     let price = document.querySelector('.price_color').innerText;
-
-    //     return {
-    //         title,
-    //         price
-    //     }
-
-    // });
-
-    const result = await page.evaluate(() => {
-        let data = []; //
-        let elements = document.querySelectorAll('.product_pod');
-
-        for (var element of elements){ // 循环
-            let title = element.childNodes[5].innerText;
-            let price = element.childNodes[7].children[0].innerText;
-
-            data.push({title, price}); // 存入数组
-        }
-
-        return data;
-    });
-
-    browser.close();
-    return result;
-};
-
-scrape().then((value) => {
-    console.log(value); // Success!
-})
-
-*/
-
 
 //-----------------------------------------------------------------------------//
 
@@ -163,12 +98,8 @@ let dev_scrape = async () => {
 
     browser.close();
     return allData;
-};
+}
 
-
-
-
-//------------------------------------------------------------------------//
 
 //不好說
 
@@ -258,8 +189,7 @@ exports.toutiao = async function (table) {
         fs.writeFile('data.txt', (JSON.stringify(data)).replace(/,/gi, "\n") + "\n", function(err) {})
     
     })
-    
-  }
+}
 
 exports.twdvd = async function (table) {
    await rr_scrape().then((data) => {
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const craw = require('./craw')
 
-// craw.toutiao('toutiao')
-craw.twdvd('eighteen')
+if(process.argv[2]){
+    let tag = process.argv[2]
+    craw.twdvd(tag)
+  } else {
+    craw.toutiao('toutiao')
+  }
